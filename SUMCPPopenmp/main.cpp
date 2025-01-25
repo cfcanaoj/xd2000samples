5a4504cf0c4f0cb3288592ac6245599933e8f506
Tomoya Takiwaki <takiwaki.tomoya@nao.ac.jp>
2025-01-25 22:00:00 +0900

    update

--- SUMCPPopenmp/main.cpp
+++ SUMCPPopenmp/main.cpp
@@ -1,4 +1,8 @@
+
 #include <omp.h>
+#include <mpi.h>
+#include <iostream>
+#include "mpiroutines.hpp"
 
 namespace basicmod{
   const int nhymax=2000;
@@ -10,26 +14,34 @@ namespace basicmod{
   const int jn = ngridy;
   const int kn = ngridz;
 
-  const int is = 1;
-  const int js = 1;
-  const int ks = 1;
+  const int is = 0;
+  const int js = 0;
+  const int ks = 0;
     
-  const int ie = ngridx;
-  const int je = ngridy;
-  const int ke = ngridz;
+  const int ie = ngridx-1;
+  const int je = ngridy-1;
+  const int ke = ngridz-1;
 
   double S[kn][jn][in];
-
+  
+  double sumall;
 
 }
 
+int setmpi(){
+  using namespace mpimod;
+  ntiles[dimx] = 4;
+  ntiles[dimy] = 2;
+  ntiles[dimz] = 1;
+  return 1;
+}
 
 int InitializeVariables(){
   using namespace basicmod;
  
-    for (int k=0; k< kn;k++){
-    for (int j=0; j< jn;j++){
-    for (int i=0; i< in;i++){
+    for (int k=ks; k<= ke;k++){
+    for (int j=js; j<= je;j++){
+    for (int i=is; i<= ie;i++){
       S[k][j][i] = 1.0;
     }
     }
@@ -41,27 +53,51 @@ int InitializeVariables(){
 
 int SumVariables(){
   using namespace basicmod;
+  using namespace mpimod;
   double sum;
+  double sumsend,sumrecv;
+  
   sum = 0.0;
 #pragma omp parallel for collapse(3) reduction(+:sum)
-  for (int k=0; k< kn;k++){
-  for (int j=0; j< jn;j++){
-  for (int i=0; i< in;i++){
+  for (int k=ks; k<= ke;k++){
+  for (int j=js; j<= je;j++){
+  for (int i=is; i<= ie;i++){
     sum = sum + S[k][j][i];
   }
   }
   }
-  
- 
+
+  sumsend = sum;
+  MPI_Allreduce(&sumsend, &sumrecv, 1, MPI_DOUBLE_PRECISION, MPI_SUM, comm3d);
+  sumall = sumrecv;
   return 1;
 }
 
 int main(){
+  using namespace std;
   using namespace basicmod;
+  double time_begin, time_end;
+  int threadsnum;
+  InitializeMPI();
+  cout<< "grid size for x y "<< ngridx << " " << ngridy << endl;
+
+  threadsnum = omp_get_max_threads();
+  cout<< "threads="<<threadsnum<< endl;
+  
   InitializeVariables();
+
+  cout<< "entering main loop" << endl;
+  time_begin = omp_get_wtime();
   for (int nhy=0;nhy<nhymax;nhy++){
     SumVariables();
   }
-    
+  time_end = omp_get_wtime();
+
+  cout<< "sum=" << sumall << endl;
+  cout<< "sim time [s] = " << time_end-time_begin << endl;
+  cout<< "time/count/cell = " << (time_end-time_begin)/(ngridx*ngridy)/nhymax << endl;
+
+  
+  cout<< "program has been finished"<<endl;
   return 1;
 }
--- SUMCPPopenmp/mpiroutines.cpp
+++ SUMCPPopenmp/mpiroutines.cpp
@@ -1,1 +1,73 @@
+#include <mpi.h>
+#include <iostream>
 
+#include "mpiroutines.hpp"
+
+namespace mpimod{
+  int myid_w, nprocs_w;
+  
+  MPI_Status stat[mreq];
+  MPI_Request req[mreq];
+  
+  MPI_Comm mpi_comm_hyd;
+  int myid_hyd, nprocs_hyd;
+  
+  MPI_Comm comm3d;
+  int myid, nprocs;
+  
+  int periodic[3];
+  int ntiles[3];
+  int coords[3];
+  
+  bool reorder;
+  int n1m, n1p, n2m, n2p, n3m, n3p;
+  int nreq, nsub;
+}
+
+
+int InitializeMPI(){
+  using namespace std;
+  using namespace mpimod;
+  int key,color;
+  int np_hyd;
+  int argc;
+  char** argv;
+
+  // Initialize MPI
+  MPI_Init(&argc, &argv);
+  MPI_Comm_size(MPI_COMM_WORLD, &nprocs_w);
+  MPI_Comm_rank(MPI_COMM_WORLD, &myid_w  );
+  // call setMPI
+  periodic[dimx]= 1;//true
+  periodic[dimy]= 1;//true
+  periodic[dimz]= 1;//true
+  if(myid_w == 0) {
+    cout <<  "MPI process=" << nprocs_w << endl;
+    cout << "decomposition=" << ntiles[dimx] <<" "<< ntiles[dimy] <<" "<< ntiles[dimz] << endl;
+  }
+
+  MPI_Bcast(ntiles,3,MPI_INTEGER,0,MPI_COMM_WORLD);
+  MPI_Bcast(periodic,3,MPI_LOGICAL,0,MPI_COMM_WORLD);
+
+  // Making 3D strucure
+  np_hyd = ntiles[dimx]*ntiles[dimy]*ntiles[dimz];
+  color = int(myid_w/np_hyd);
+  key   = myid_w ;
+  MPI_Comm_split(MPI_COMM_WORLD,color,key,&mpi_comm_hyd);
+  MPI_Comm_size( mpi_comm_hyd, &nprocs_hyd);
+  MPI_Comm_rank( mpi_comm_hyd, &myid_hyd)  ;   
+  
+  // Create a virtual Cartesian topology for the domain decomposition.
+  MPI_Cart_create( mpi_comm_hyd, 3, ntiles, periodic, reorder, &comm3d);
+  MPI_Comm_rank( comm3d, &myid);
+  MPI_Comm_size( comm3d, &nprocs);
+
+  // Find the ranks of my neighbors; find my virtual Cartesian coords.
+  MPI_Cart_shift( comm3d, dimx, 1, &n1m, &n1p);
+  MPI_Cart_shift( comm3d, dimy, 1, &n2m, &n2p);
+  MPI_Cart_shift( comm3d, dimz, 1, &n3m, &n3p);
+  
+  MPI_Cart_coords( comm3d, myid, 3, coords);
+      
+  return 1;
+  }
